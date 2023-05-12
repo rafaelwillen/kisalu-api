2449201dfa575c01251f5b4e810a1d3273fc619e
Rafael Willen <rafaelpadre20@gmail.com>
2023-05-12 12:02:15 +0100

    Config firebase

---
+++ src/configs/firebase.ts
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+import { getStorage } from "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDIDs4GvSPfvx8IgMpYVMV3Fsz0aJHS_W0",
+  authDomain: "kisalu-server.firebaseapp.com",
+  projectId: "kisalu-server",
+  messagingSenderId: "481448767753",
+  appId: "1:481448767753:web:862e5f2d0d6a7fac1863d8",
+  storageBucket: "gs://kisalu-server.appspot.com/",
+};
+
+const app = initializeApp(firebaseConfig);
+export const storage = getStorage(app);
