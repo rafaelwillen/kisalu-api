ac6eedc054dd9430f20dc21f67e0a060cf05537f
Rafael Willen <rafaelpadre20@gmail.com>
2023-05-13 00:22:26 +0100

    Remove PORT env var

--- src/configs/index.ts
+++ src/configs/index.ts
@@ -1,9 +1,7 @@
 import { FastifyInstance } from "fastify";
 import configCORS from "./cors";
-import configEnv from "./env";
 
 async function initConfig(fastify: FastifyInstance) {
-  await configEnv(fastify);
   await configCORS(fastify);
 }
 
