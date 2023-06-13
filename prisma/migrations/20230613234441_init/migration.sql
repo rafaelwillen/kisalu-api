e1e0a8426e108ee4a3b4627716c309d32b9cf98c
Rafael Willen <rafaelpadre20@gmail.com>
2023-06-14 00:52:48 +0100

    Add migration

---
+++ prisma/migrations/20230613234441_init/migration.sql
@@ -0,0 +1,277 @@
+-- CreateEnum
+CREATE TYPE "Gender" AS ENUM ('Male', 'Female');
+
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('Administrator', 'Provider', 'Client');
+
+-- CreateEnum
+CREATE TYPE "ActivityState" AS ENUM ('Active', 'OnHold', 'OnDispute', 'Finished', 'Cancelled', 'OnRevision');
+
+-- CreateEnum
+CREATE TYPE "State" AS ENUM ('Available', 'Draft', 'Unavailable');
+
+-- CreateEnum
+CREATE TYPE "DisputeStatus" AS ENUM ('UnderReview', 'ResolutionProposed', 'Resolved', 'Escalated');
+
+-- CreateEnum
+CREATE TYPE "FileType" AS ENUM ('Document', 'Image');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "firstName" TEXT NOT NULL,
+    "lastName" TEXT NOT NULL,
+    "avatarImageURL" TEXT NOT NULL,
+    "biography" TEXT NOT NULL,
+    "birthDate" TIMESTAMP(3) NOT NULL,
+    "gender" "Gender" NOT NULL,
+    "loginId" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Login" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "phoneNumber" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "isActive" BOOLEAN NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "role" "Role" NOT NULL,
+
+    CONSTRAINT "Login_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Address" (
+    "id" TEXT NOT NULL,
+    "county" TEXT NOT NULL,
+    "province" TEXT NOT NULL,
+    "addressLine" TEXT NOT NULL,
+    "coordinatesId" TEXT NOT NULL,
+    "activityId" TEXT,
+
+    CONSTRAINT "Address_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Coordinates" (
+    "id" TEXT NOT NULL,
+    "latitude" TEXT NOT NULL,
+    "longitude" TEXT NOT NULL,
+
+    CONSTRAINT "Coordinates_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Bidding" (
+    "id" TEXT NOT NULL,
+    "proposedValue" INTEGER NOT NULL,
+    "estimatedDeliveryDate" TIMESTAMP(3) NOT NULL,
+    "coverLetter" TEXT NOT NULL,
+    "providerUserId" TEXT,
+    "projectId" TEXT,
+
+    CONSTRAINT "Bidding_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "ProviderExpirienceInfo" (
+    "id" TEXT NOT NULL,
+    "institutionName" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "startDate" TIMESTAMP(3) NOT NULL,
+    "endDate" TIMESTAMP(3),
+    "providerUserId" TEXT,
+
+    CONSTRAINT "ProviderExpirienceInfo_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Portfolio" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "projectLink" TEXT NOT NULL,
+    "projectImageURL" TEXT[],
+    "providerUserId" TEXT,
+
+    CONSTRAINT "Portfolio_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Review" (
+    "id" TEXT NOT NULL,
+    "rating" INTEGER NOT NULL,
+    "commentary" TEXT NOT NULL,
+    "clientUserId" TEXT,
+
+    CONSTRAINT "Review_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Activity" (
+    "id" TEXT NOT NULL,
+    "agreedValue" INTEGER NOT NULL,
+    "state" "ActivityState" NOT NULL,
+    "startDate" TIMESTAMP(3) NOT NULL,
+    "estimatedFinishDate" TIMESTAMP(3) NOT NULL,
+    "finishedDate" TIMESTAMP(3) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "clientUserId" TEXT,
+    "providerUserId" TEXT,
+    "linkedServiceId" TEXT,
+    "linkedProjectId" TEXT,
+
+    CONSTRAINT "Activity_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Service" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "bannerImageURL" TEXT,
+    "publishedDate" TIMESTAMP(3) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "viewsCount" INTEGER NOT NULL DEFAULT 0,
+    "state" "State" NOT NULL,
+    "featuredImagesURL" TEXT[],
+    "minimumPrice" INTEGER NOT NULL,
+    "isHighlighted" BOOLEAN NOT NULL,
+    "categoryId" TEXT,
+
+    CONSTRAINT "Service_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Dispute" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "reason" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "resolution" TEXT,
+    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "status" "DisputeStatus" NOT NULL,
+    "adminUserId" TEXT,
+    "activityId" TEXT NOT NULL,
+
+    CONSTRAINT "Dispute_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Attachment" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "fileType" "FileType" NOT NULL,
+    "url" TEXT NOT NULL,
+    "disputeId" TEXT,
+
+    CONSTRAINT "Attachment_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Category" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "mainImageURL" TEXT NOT NULL,
+    "bannerImageURL" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "creatorAdminId" TEXT,
+
+    CONSTRAINT "Category_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Project" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "bannerImageURL" TEXT,
+    "publishedDate" TIMESTAMP(3) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "viewsCount" INTEGER NOT NULL DEFAULT 0,
+    "state" "State" NOT NULL,
+    "featuredImagesURL" TEXT[],
+    "minimumPrice" INTEGER NOT NULL,
+    "maximumPrice" INTEGER NOT NULL,
+    "categoryId" TEXT,
+
+    CONSTRAINT "Project_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_loginId_key" ON "User"("loginId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Login_email_key" ON "Login"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Login_phoneNumber_key" ON "Login"("phoneNumber");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Address_coordinatesId_key" ON "Address"("coordinatesId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Address_activityId_key" ON "Address"("activityId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Dispute_activityId_key" ON "Dispute"("activityId");
+
+-- AddForeignKey
+ALTER TABLE "User" ADD CONSTRAINT "User_loginId_fkey" FOREIGN KEY ("loginId") REFERENCES "Login"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Address" ADD CONSTRAINT "Address_coordinatesId_fkey" FOREIGN KEY ("coordinatesId") REFERENCES "Coordinates"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Address" ADD CONSTRAINT "Address_activityId_fkey" FOREIGN KEY ("activityId") REFERENCES "Activity"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Bidding" ADD CONSTRAINT "Bidding_providerUserId_fkey" FOREIGN KEY ("providerUserId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Bidding" ADD CONSTRAINT "Bidding_projectId_fkey" FOREIGN KEY ("projectId") REFERENCES "Project"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ProviderExpirienceInfo" ADD CONSTRAINT "ProviderExpirienceInfo_providerUserId_fkey" FOREIGN KEY ("providerUserId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Portfolio" ADD CONSTRAINT "Portfolio_providerUserId_fkey" FOREIGN KEY ("providerUserId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Review" ADD CONSTRAINT "Review_clientUserId_fkey" FOREIGN KEY ("clientUserId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Activity" ADD CONSTRAINT "Activity_clientUserId_fkey" FOREIGN KEY ("clientUserId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Activity" ADD CONSTRAINT "Activity_providerUserId_fkey" FOREIGN KEY ("providerUserId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Activity" ADD CONSTRAINT "Activity_linkedServiceId_fkey" FOREIGN KEY ("linkedServiceId") REFERENCES "Service"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Activity" ADD CONSTRAINT "Activity_linkedProjectId_fkey" FOREIGN KEY ("linkedProjectId") REFERENCES "Project"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Service" ADD CONSTRAINT "Service_categoryId_fkey" FOREIGN KEY ("categoryId") REFERENCES "Category"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Dispute" ADD CONSTRAINT "Dispute_adminUserId_fkey" FOREIGN KEY ("adminUserId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Dispute" ADD CONSTRAINT "Dispute_activityId_fkey" FOREIGN KEY ("activityId") REFERENCES "Activity"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Attachment" ADD CONSTRAINT "Attachment_disputeId_fkey" FOREIGN KEY ("disputeId") REFERENCES "Dispute"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Category" ADD CONSTRAINT "Category_creatorAdminId_fkey" FOREIGN KEY ("creatorAdminId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Project" ADD CONSTRAINT "Project_categoryId_fkey" FOREIGN KEY ("categoryId") REFERENCES "Category"("id") ON DELETE SET NULL ON UPDATE CASCADE;
