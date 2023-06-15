16af95956b3ef67ddaabda41d3141eb0c13a19c0
Rafael Willen <rafaelpadre20@gmail.com>
2023-06-15 11:05:43 +0100

    Refactor the upload service

--- src/services/UploadService.ts
+++ src/services/UploadService.ts
@@ -1,6 +1,7 @@
 import { storage } from "@/configs/firebase";
+import { HTTP_STATUS_CODE } from "@/constants";
+import HTTPError from "@/utils/error/HTTPError";
 import { imageMimetypeRegex } from "@/utils/regex";
-import { RequestFileTooLargeError } from "@fastify/multipart";
 import { FastifyReply, FastifyRequest } from "fastify";
 import { FirebaseError } from "firebase/app";
 import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
@@ -10,12 +11,11 @@ import { extname } from "node:path";
 export default class UploadService {
   async uploadCategoryBanner(request: FastifyRequest, reply: FastifyReply) {
     const data = await request.file();
-    if (!data) {
-      return reply.code(400).send({ message: "No file received" });
-    }
+    if (!data)
+      throw new HTTPError(HTTP_STATUS_CODE.BAD_REQUEST, "No File received");
     const isValidMimetype = imageMimetypeRegex.test(data.mimetype);
     if (!isValidMimetype)
-      return reply.code(400).send({ message: "Invalid file type" });
+      throw new HTTPError(HTTP_STATUS_CODE.BAD_REQUEST, "Invalid file type");
     const extension = extname(data.filename);
     const fileName = randomUUID().concat("_category").concat(extension);
     try {
@@ -24,23 +24,31 @@ export default class UploadService {
         data.mimetype,
         await data.toBuffer()
       );
-      reply.code(201).send({
+      reply.code(HTTP_STATUS_CODE.CREATED).send({
         url,
       });
     } catch (error) {
-      console.log(error);
-      if (error as RequestFileTooLargeError)
-        reply.code(413).send({ message: "File too large" });
       if (error instanceof FirebaseError) {
-        reply.code(500).send({ message: error.message, code: error.code });
-      } else reply.code(500).send({ message: "Internal server error" });
+        if (error.code === "storage/unauthorized")
+          throw new HTTPError(
+            HTTP_STATUS_CODE.UNAUTHORIZED,
+            "Unauthorized",
+            error
+          );
+        reply
+          .code(HTTP_STATUS_CODE.INTERNAL_SERVER_ERROR)
+          .send({ message: error.message, code: error.code });
+      } else
+        reply
+          .code(HTTP_STATUS_CODE.INTERNAL_SERVER_ERROR)
+          .send({ message: "Internal server error" });
     }
   }
 }
 
 async function upload(path: string, contentType: string, data: Buffer) {
   const fileRef = ref(storage, path);
-  await uploadBytes(fileRef, await data, {
+  await uploadBytes(fileRef, data, {
     contentType,
   });
   const url = await getDownloadURL(fileRef);
