72c43dfb54ef66c5776bae247ff5f78bdc13c23d
Rafael Willen <rafaelpadre20@gmail.com>
2023-06-16 01:00:45 +0100

    Change the cors config

--- src/configs/cors.ts
+++ src/configs/cors.ts
@@ -2,7 +2,13 @@ import fastifyCors from "@fastify/cors";
 import { FastifyInstance } from "fastify";
 
 async function configCORS(fastify: FastifyInstance) {
-  fastify.register(fastifyCors);
+  fastify.register(fastifyCors, {
+    origin:
+      process.env.NODE_ENV === "production"
+        ? "https://kisalu.live"
+        : "http://localhost:3000",
+    credentials: true,
+  });
 }
 
 export default configCORS;
