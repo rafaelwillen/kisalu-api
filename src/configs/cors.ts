e9f45f3e576af03ecdf1409661ec77719341b9ec
Rafael Willen <rafaelpadre20@gmail.com>
2023-06-20 23:45:53 +0100

    Add origin for vercel preview deployments

--- src/configs/cors.ts
+++ src/configs/cors.ts
@@ -3,10 +3,11 @@ import { FastifyInstance } from "fastify";
 
 async function configCORS(fastify: FastifyInstance) {
   fastify.register(fastifyCors, {
-    origin:
-      process.env.NODE_ENV === "production"
-        ? "https://kisalu.live"
-        : "http://localhost:3000",
+    origin: [
+      /^http:\/\/localhost:/,
+      "https://kisalu.live",
+      /rafaelwillen\.vercel\.app$/,
+    ],
     credentials: true,
   });
 }
