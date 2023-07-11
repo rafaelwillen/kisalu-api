2e448313d43dd033c47ce15b1668690e95c0b272
Rafael Willen <rafaelpadre20@gmail.com>
2023-07-11 15:25:44 +0100

    Set the user as inactive by default

--- src/repository/UserRepository.ts
+++ src/repository/UserRepository.ts
@@ -27,7 +27,7 @@ export default class UserRepository extends Repository {
         auth: {
           create: {
             ...data.auth,
-            isActive: true,
+            isActive: false,
             role,
           },
         },
